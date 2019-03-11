5f255ebee06b674a7c197a19c0c3a927f6f3a900
jelrad11 <31778964+jelrad11@users.noreply.github.com>
2019-03-11 05:18:03 +0100

    Final Countdown

--- Assets/Scripts/Char01_Animation.cs
+++ Assets/Scripts/Char01_Animation.cs
@@ -7,10 +7,24 @@ public class Char01_Animation : MonoBehaviour
     public Animator _animator;
     public bool running;
 
+    public bool crawling;
+
 
     void Start()
     {
         _animator = GetComponent<Animator>();
         _animator.SetBool("Running", true);          
+    }
+
+    void Update()
+    {
+        if(crawling)
+        {
+            _animator.SetBool("crawling", true);
+        }
+        else
+        {
+          _animator.SetBool("crawling", false);  
+        }
     }
 }
